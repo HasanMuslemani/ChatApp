00301b71dfb2601bd5ae0b961b412ddf43a1989f
Hasan Muslemani <w0721766@myscc.ca>
2021-07-14 13:58:44 -0400

    #4 Display list of users to chat with
    * Created custom user item for recyclerview
    
    * Created custom user adapter for recyclerview
    
    * Grabbing all users from firebase to display in RecyclerView

---
+++ app/src/main/java/com/example/chatapp/Adapters/UserAdapter.java
@@ -0,0 +1,64 @@
+package com.example.chatapp.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.chatapp.Models.User;
+import com.example.chatapp.R;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UserAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+    private List<User> users;
+
+    public UserAdapter(Context context, List<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.user_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        User user = users.get(position);
+        ((ViewHolder) holder).username.setText(user.getUsername());
+        if(user.getImageURL().equals("default")) {
+            ((ViewHolder) holder).profileImage.setImageResource(R.mipmap.ic_launcher);
+        } else {
+            Glide.with(context).load(user.getImageURL()).into(((ViewHolder) holder).profileImage);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public ImageView profileImage;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profileImage = itemView.findViewById(R.id.profileImage);
+        }
+    }
+}
